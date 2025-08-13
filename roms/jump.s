7e6bea0b2cb972dec84f7bce526545ca6aafc197
Martin Sauve <martin@billesandco.paris>
2025-08-13 15:11:22 +0200

    add roms to project

---
+++ roms/jump.s
@@ -0,0 +1,17 @@
+      .org $0600
+      LDX #0
+   LOOP:
+      LDA MESSAGE,X
+      BEQ DONE
+      JSR PUTCHAR
+      INX
+      JMP LOOP
+   DONE:
+      NOP
+      BRK
+   PUTCHAR:
+      STA $F001 ;should be captured by the emulator!
+      RTS
+
+   MESSAGE:
+      .text "Hello, World!\n"
---
+++ roms/stack.s
@@ -0,0 +1,14 @@
+      .org $0600
+      LDX #0
+   LOOP:
+      LDA MESSAGE,X
+      BEQ DONE
+      PHA
+      ;STA $F001 ;should be captured by the emulator!
+      INX
+      JMP LOOP
+   DONE:
+      NOP
+      BRK
+   MESSAGE:
+      .text "Hello, World!\n"
